b7143777f5760013ef4e1c046ffa18b391799c8d
Igor Khomenko <xomigrek@gmail.com>
2022-07-10 15:37:45 +0300

    UPD commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int cost = 5;
+        int cost = 1000_000;
         int bonusMile = 20;
-        int bonus = cost/bonusMile;
-        System.out.println(bonus);
+        int bonus = cost / bonusMile;
+        System.out.println("Начислено " + bonus + " бонус(ов)");
 
     }
 }
